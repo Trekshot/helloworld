42347b8c29fff40c2606310205c7a91c814293ad
drodata <drodata@foxmail.com>
2014-10-24 20:12:19 +0800

    Check managing multiple respositories

--- fix1.php
+++ fix1.php
@@ -2,3 +2,10 @@
 echo "go";
 ?>
 ddd
+If there are more than one respositories, when I run 
+
+	git push orign master
+
+How does GitHub recognize which respository I'm pushing?
+
+This commit check it.
